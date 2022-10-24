3cd19a4d2e02c86dc285d36ae06a951fc3d7d373
MartinelliFabio <fabio.martinelli996@gmail.com>
2022-10-24 16:35:51 +0200

    Create the post creation function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+/*  -----------------------------------------------------------------------------------------------
+    Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+    Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post.
+    Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+    - id del post, numero progressivo da 1 a n
+    - nome autore,
+    - foto autore,
+    - data in formato americano (mm-gg-yyyy),
+    - testo del post,
+    - immagine (non tutti i post devono avere una immagine),
+    - numero di likes.
+    *Non è necessario creare date casuali*
+    *Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)*
+    Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+    Milestone 3- Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+    Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+    BONUS
+    1. Formattare le date in formato italiano (gg/mm/aaaa)
+    2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
+    3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
+--------------------------------------------------------------------------------------------------- */
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +77,50 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+// Prendo il div#container dal DOM
+const containerHTML = document.getElementById('container');
+
+createPost(); // Richiamo la funzione per la creazione dei Post
+
+// Funzione per la creazione dei Post
+function createPost(){
+    posts.forEach((element) => {
+
+        // Creo il div con tutto il contenuto dei post
+        const post = document.createElement('div');
+        post.innerHTML = `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src="${element.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`
+
+        containerHTML.append(post); // Appendo il post al container
+    });
+}
